e7176e4a8a00b4a97d8dfd7c54f3b4bf43965f71
Sjoerd Dijkstra <sjoerd@yogy.io>
2023-02-15 18:49:08 +0100

    Remove newline

--- scripts/update-formula.sh
+++ scripts/update-formula.sh
@@ -29,7 +29,6 @@ log_std "Sha is ${GREEN}${sha}"
 [[ -z "${version}" ]] && exit 1
 [[ -z "${sha}" ]] && exit 1
 
-
 sed -E -i \
   -e "s/v[0-9]+.[0-9]+.[0-9]+/${version}/g" \
   -e "s/sha256 \"[0-9a-z]+\"/sha256 \"${sha}\"/" \
